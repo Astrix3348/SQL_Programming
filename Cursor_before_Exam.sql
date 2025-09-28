2de55e1051bd59b184cbc4b7bf480f25b2c28f05
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-09-28 17:45:39 +0530

    Update Cursor_before_Exam.sql

--- Cursor_before_Exam.sql
+++ Cursor_before_Exam.sql
@@ -1,4 +1,6 @@
 
+use exam;
+ 
 CREATE TABLE Students (
     StudentID INT PRIMARY KEY,
     Name VARCHAR(50),
@@ -73,6 +75,117 @@ select * from dets;
 end // 
 
 call get_grade();
+
+
+CREATE TABLE Employee (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    basic_pay DECIMAL(10,2)
+);
+
+INSERT INTO Employee VALUES
+(101, 'Alice', 1200),
+(102, 'Bob', 1500),
+(103, 'Charlie', 900),
+(104, 'David', 2000),
+(105, 'Eve', 1800);
+
+-- Q2
+drop procedure if exists get_details;
+delimiter //
+create procedure get_details()
+begin
+declare done int default 0;
+declare e_name varchar(20);
+declare bonus int default 0;
+
+declare c1 cursor for select emp_name, basic_pay from Employee;
+declare continue handler for not found set done = 1;
+
+drop temporary table if exists display;
+create temporary table display(t_name varchar(20), t_bonus int);
+
+open c1;
+
+read_details : loop
+   fetch c1 into e_name, bonus;
+   if done = 1 then 
+   leave read_details;
+   end if;
+   
+   if(bonus > 1500) then
+   set bonus = bonus + (bonus*0.1);
+   else
+   set bonus = bonus + (bonus*0.05);
+   end if;
+
+insert into display values (e_name, bonus);
+
+end loop;
+close c1;
+select * from display;
+end //
+
+call get_details();
+
+
+-- Q3
+CREATE TABLE EmployeeExperience (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    experience_years INT
+);
+
+INSERT INTO EmployeeExperience VALUES
+(101, 'Alice', 2),
+(102, 'Bob', 5),
+(103, 'Charlie', 7),
+(104, 'David', 1),
+(105, 'Eve', 10);
+
+
+
+drop procedure if exists get_pos;
+delimiter //
+create procedure get_pos()
+begin
+declare domain varchar(20);
+declare new_name varchar(20);
+declare years int;
+declare done int default 0;
+
+declare c1 cursor for select emp_name, experience_years from EmployeeExperience;
+declare continue handler for not found set done = 1;
+
+drop temporary table if exists t_det;
+create temporary table t_det(name varchar(20), desig varchar(20));
+
+open c1;
+
+read_dets : loop
+
+fetch c1 into new_name, years;
+if done = 1 then 
+leave read_dets;
+end if;
+
+  if(years>=8) then
+     set domain = "Expert";
+  elseif(years >= 5 and years <= 7) then
+     set domain = "Intermediate";
+  else
+     set domain = "Beginner";
+  end if;
+
+insert into t_det values (new_name, domain);
+
+end loop;
+close c1;
+select * from t_det;
+end //
+
+call get_pos();
+
 
 
 
